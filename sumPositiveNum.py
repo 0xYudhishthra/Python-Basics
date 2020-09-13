e12ac6086b8ff19b498b61803fb454bddaeca5fb
Tectrix-tech <66850474+Tectrix-tech@users.noreply.github.com>
2020-09-13 20:10:52 +0800

    Create sumPositiveNum.py

---
+++ sumPositiveNum.py
@@ -0,0 +1,7 @@
+def sum_positive_numbers(n):
+  if n<1:
+    return n 
+  return n + sum_positive_numbers(n-1)
+  
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
