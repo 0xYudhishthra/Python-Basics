00f0420cbfadacf8c661c365bdd79ba4318e2f07
Tectrix-tech <66850474+Tectrix-tech@users.noreply.github.com>
2020-09-13 18:08:03 +0800

    Create sumOfDivisors.py

---
+++ sumOfDivisors.py
@@ -0,0 +1,12 @@
+def sum_divisors(n):
+    return sum([i for i in range(1, n)
+                if n % i == 0])
+
+print(sum_divisors(0))
+# 0
+print(sum_divisors(3)) # Should sum of 1
+# 1
+print(sum_divisors(36)) # Should sum of 1+2+3+4+6+9+12+18
+# 55
+print(sum_divisors(102)) # Should be sum of 2+3+6+17+34+51
+# 114
