f329d1fd52660268c4dd9d09b1bafb5b7c5b5fe2
Tectrix-tech <66850474+Tectrix-tech@users.noreply.github.com>
2020-09-14 17:13:33 +0800

    Create replaceWord.py

---
+++ replaceWord.py
@@ -0,0 +1,6 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+newfilenames=[file.replace(".hpp",".h") for file in filenames] 
+print(newfilenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
